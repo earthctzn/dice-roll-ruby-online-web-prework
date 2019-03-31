1dae4d6ab403df983fb9a52aae8b5dedaf91ae20
Caleb Aviles <earthctzn1@gmail.com>
2019-03-31 19:14:27 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,6 +2,10 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
-def roll
-  rand(1..6)# code goes here
+#def roll
+ # rand(1..6)# code goes here
+end
+
+def second_roll
+  rand[1, 2, 3, 4, 5, 6,]
 end
